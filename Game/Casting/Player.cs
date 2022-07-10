ce0c8429d6d2f19268411bc17f65a126b5f6627b
prattbj <104381242+prattbj@users.noreply.github.com>
2022-07-09 18:15:54 -0600

    Fixed spawn

--- Game/Casting/Player.cs
+++ Game/Casting/Player.cs
@@ -4,13 +4,11 @@ namespace Unit06.Game.Casting
     /// <para>The bike continually leaves a trail behind it..</para>
     public class Player : Actor
     {
-        private Point spawn = new Point(0, 0);
-
-
         /// Constructs a new instance of Player.
         public Player(Sprite sprite, Point spawn)
         {
-            this.spawn = spawn;
+            SetPosition(spawn);
+
             SetSprite(sprite);
         }
     }

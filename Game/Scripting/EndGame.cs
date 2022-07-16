bc6f374b8811624595172db7e8f8e659511ae40c
edw17003 <edw17003@byui.edu>
2022-07-16 17:09:57 -0600

    dumb merge conflicts

--- Game/Scripting/EndGame.cs
+++ Game/Scripting/EndGame.cs
@@ -29,20 +29,11 @@ namespace Unit06.Game.Scripting
                 if (player.GetHealth() <= 0)
                 {
                     audioService.StopMusic(new Casting.Sound("Game/Assets/Music\\music.mp3"));
-<<<<<<< HEAD
                     
                     
                     script.RemoveAllOfKey("update");
                     script.RemoveAllOfKey("input");
                     script.RemoveAllOfKey("sound");
-=======
-                    script.RemoveAction("input", script.GetActions("input")[0]);
-                    script.RemoveAction("update", script.GetActions("update")[4]);
-                    script.RemoveAction("update", script.GetActions("update")[3]);
-                    script.RemoveAction("update", script.GetActions("update")[2]);
-                    script.RemoveAction("update", script.GetActions("update")[1]);
-                    script.RemoveAction("update", script.GetActions("update")[0]);
->>>>>>> 86349874bd2b60e19b5d6ea403d98b2cc2c8b549
                     script.RemoveAction("output", script.GetActions("output")[0]);
                     script.RemoveAction("endgame", script.GetActions("endgame")[0]);
                     script.AddAction("output", new DrawTitle(videoService, gamepadService, audioService, "Game Over. Press Start to Play Again."));

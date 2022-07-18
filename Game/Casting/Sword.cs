4624b1d38746eeac6abe0ec385891842e538fca6
prattbj <104381242+prattbj@users.noreply.github.com>
2022-07-18 10:30:46 -0600

    Added sword comments

--- Game/Casting/Sword.cs
+++ Game/Casting/Sword.cs
@@ -1,18 +1,18 @@
 namespace Unit06.Game.Casting
 {
-    /// <para>A player on a bike.</para>
-    /// <para>The bike continually leaves a trail behind it..</para>
+    /// <para>A sword to be held or thrown</para>
+    
     public class Sword : Actor
     { 
 
-        /// Constructs a new instance of Player.
+        
         private Sprite sprite = new Sprite("Game/Assets/Sprites/sword.png", 1, 180);
         private int cooldown = 0;
         private int cdStart = 120;
         private int damage = 25;
         private int[] neutralSize = new int[] {64, 10};
         private bool thrown = false;
-        
+        /// Constructs a new instance of Sword.
         public Sword()
         {
             
@@ -20,17 +20,17 @@ namespace Unit06.Game.Casting
             //SetSize(neutralSize[0], neutralSize[1]);
             
         }
-
+        //Sets the current rotation of the Sword's sprite
         public void SetSpriteRotation(int rotate)
         {
             this.sprite.SetRotation(rotate);
         }
-
+        //Returns the size of the neutral state of the sprite
         public int[] GetNeutralSize()
         {
             return this.neutralSize;
         }
-
+        //Checks for collision
         public override bool Overlaps(Actor other)
         {
             float x0;
@@ -56,7 +56,7 @@ namespace Unit06.Game.Casting
             return (x0 < other.GetRight() && x1 > other.GetLeft()
                  && y1 < other.GetBottom() && y0 > other.GetTop());
         }
-
+        //Sets if the sword is currently being thrown
         public void SetIsThrown(bool thrown)
         {
             this.thrown = thrown;
@@ -65,12 +65,12 @@ namespace Unit06.Game.Casting
                 StartCooldown();
             } 
         }
-
+        //Returns if the sword is being thrown
         public bool GetIsThrown()
         {
             return this.thrown;
         }
-
+        //Decrements the sword throw's cooldown
         public void SetCooldown()
         {
             if (cooldown > 0) {
@@ -82,20 +82,22 @@ namespace Unit06.Game.Casting
             //     this.thrown = false;
             // }
         }
-
+        //Returns the value of the sword's current max cooldown value
         public int GetcdStart()
         {
             return this.cdStart;
         }
-
+        //Returns the sword's current cooldown value
         public int GetCooldown()
         {
             return this.cooldown;
         }
+        //Starts the sword throw cooldown
         private void StartCooldown()
         {
             this.cooldown = cdStart;
         }
+        //Was going to decrease the velocity of the sword when it is in the air. Not used currently.
         public void DecreaseVelocity()
         {
             if (cooldown > 0)
@@ -103,10 +105,12 @@ namespace Unit06.Game.Casting
             // SetVelocity(new Point((int)(GetVelocity().GetX() - cdStart/(cooldown+1)), (int)(GetVelocity().GetY() - cdStart/(cooldown + 1))));
             }
         }
+        //Returns the amount of damage the sword does
         public int GetDamage()
         {
             return this.damage;
         }
+        //Increases the amount of damage the sword does
         public void SetDamage()
         {
             damage ++;

2e0f93c917ca48c56c6ccafe354977004b1f66ad
prattbj <104381242+prattbj@users.noreply.github.com>
2022-07-18 10:33:34 -0600

    Added comments to player

--- Game/Casting/Player.cs
+++ Game/Casting/Player.cs
@@ -1,7 +1,7 @@
 namespace Unit06.Game.Casting
 {
-    /// <para>A player on a bike.</para>
-    /// <para>The bike continually leaves a trail behind it..</para>
+    /// <para>A player that moves</para>
+    
     public class Player : Actor
     {
         private int cooldown = 0;
@@ -19,20 +19,22 @@ namespace Unit06.Game.Casting
             SetSize(64, 64);
             this.color = color;
         }
+        //Returns the color of the player
         public string GetPlayerColor()
         {
             return color;
         }
+        //Sets the current angle of the player
         public void SetAngle(int angle)
         {
             this.angle = angle;
         }
-
+        //Gets the current angle of the player
         public int GetAngle()
         {
             return this.angle;
         }
-
+        //Decrements the hit cooldown of the player
         public void SetCooldown()
         {
             if (cooldown > 0)
@@ -40,26 +42,27 @@ namespace Unit06.Game.Casting
                 cooldown--;
             }
         }
-
+        //Starts the cooldown for the player not being able to be hit
         public void StartCooldown()
         {
             this.cooldown = cdStart;
         }
-
+        //Returns the current cooldown value
         public int GetCooldown()
         {
             return this.cooldown;
         }
-
+        //Returns the player's current health
         public int GetHealth()
         {
             return this.health;
         }
-
+        //Decreases the player's current hp
         public void DamagePlayer(int damage)
         {
             this.health -= damage;
         }
+        //Heals the player
         public void HealPlayer(int healing)
         {
             this.health += healing;

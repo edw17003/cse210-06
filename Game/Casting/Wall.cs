8e35b6d23fa3fb8568a2b78d7f82329c6b1ffe8e
prattbj <104381242+prattbj@users.noreply.github.com>
2022-07-18 10:34:39 -0600

    Added comments to Wall

--- Game/Casting/Wall.cs
+++ Game/Casting/Wall.cs
@@ -6,6 +6,7 @@ namespace Unit06.Game.Casting
         private int posY;
         private int width;
         private int height;
+        //Constructs a wall
         public Wall(int posX, int posY, int width, int height)
         {
             this.posX = posX;
@@ -16,18 +17,22 @@ namespace Unit06.Game.Casting
             this.SetPosition(new Point(posX, posY));
         }
         
+        //Returns the X position of the wall
         public int GetPosX()
         {
             return posX;
         }
+        //Returns the Y position of the wall
         public int GetPosY()
         {
             return posY;
         }
+        //Returns the wall's width
         public int GetWidth()
         {
             return width;
         }
+        //Returns the wall's height
         public int GetHeight()
         {
             return height;
